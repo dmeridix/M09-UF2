4b77d152df95af0cdc560cbda8cb4a7011f04421
Daniel Merida <thos.merida.dani@gmail.com>
2024-12-04 20:16:50 +0100

    primer push

--- 01-Fils/Principal.java
+++ 01-Fils/Principal.java
@@ -12,7 +12,7 @@ public class Principal {
             juan.join();
             pepe.join();
         } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
+            System.out.println("Error al aplicar el join " + e);
         }
     }
 }
